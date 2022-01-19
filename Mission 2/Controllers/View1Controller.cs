f5c70cb4e6c83cef329b08f2d7afc490585d0fb1
Ethritch <ethritch@gmail.com>
2022-01-18 22:41:49 -0700

    mission 3 homework

---
+++ Mission 2/Controllers/View1Controller.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_2.Controllers
+{
+    public class View1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult grade_calc()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult grade_calc(grade_calc_model model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission 2/Models/grade_calc_model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission_2.Models
+{
+    public class grade_calc_model
+    {
+        [Range(0, 1, ErrorMessage = "Value must be a decimal between 0 and 1")]
+        public decimal assignment { get; set; }
+
+        public decimal gp { get; set; }
+
+        public decimal qz { get; set; }
+
+        public decimal exam { get; set; }
+
+        public decimal intex { get; set; }
+        
+    }
+}
